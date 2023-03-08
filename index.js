44fce89dfa3dbfe980acd1943bf648251e18c9d0
Tolu Akinrinola <remmakins@gmail.com>
2023-03-07 22:44:30 -0500

    complete lab

---
+++ index.js
@@ -0,0 +1,18 @@
+function introduction(name) {
+    return `Hi, my name is ${name}.`;
+
+}
+
+
+function introductionWithLanguage(name, language) {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+
+}
+function sayHelloTo(firstName = "user") {
+    return `Hello, ${firstName}!`;
+
+}
+
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
